62d9cd7ff9f15a8253092c38fc57e6f79046c6d9
koodalrasu <prabinkoodal@gmail.com>
2025-08-01 18:28:46 +0530

    succes

---
+++ 1.py
@@ -0,0 +1,21 @@
+import pandas as kk
+import numpy as np
+a=kk.array([1,2,"3"])
+print(a.ndim)
+b=np.zeros([2,3])
+c=np.arange(1,20)
+e=np.linspace(10,1,5)
+print(b)
+print(c)
+print(e)
+
+d=np.array([10,20,30,40,50])
+print(np.mean(d))
+
+aa=np.full([2,3],7)
+print(aa)
+k=np.array([1,2,3])
+j=np.array([1,2,3])
+print(k%j)
+
+import pandas as ka
